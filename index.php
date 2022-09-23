028da1043da63f3d71bfc8bdf0833b3bdcf9fc8e
ramadhan <ramadanker266@gmail.com>
2022-09-23 08:43:38 +0700

    Manambahkan login , logout , register .

--- index.php
+++ index.php
@@ -33,8 +33,9 @@ if (isset($_POST["cari"])) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login/colorlib.com/etc/lf/Login_v3/index.html">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
                     <li class=" nav-item"><a class="nav-link" href="register.php">Register</a></li>
+                    <li class=" nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
 
                 </ul>
             </div>
